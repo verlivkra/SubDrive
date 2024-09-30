3d90f31c6904f09bce03c6678326f1bb29b11fc8
Veronica Liverud Krathe <veronlk@ntnu.no>
2024-09-30 11:59:49 +0200

    Function for SubDyn tower only (ElastoDyn drivetrian)

--- Python/cases.py
+++ Python/cases.py
@@ -83,6 +83,24 @@ def floating(script_dir, tower_type, bedplate_type, shaft_type, mb_type, add_nac
     mymodel.writeModel()
     mymodel.writeJSON()
 
+def floating_towerOnly(script_dir, tower_type, bedplate_type, shaft_type, mb_type, add_nacelle_yaw_inertia = True, add_yaw_br_mass = True):
+    print('Building floating platform')
+    mymodel = build.Model(script_dir, platform_type='floating', interface_location='tower')
+    mymodel.timeStep()
+    mymodel.floatingPlatform()
+    mymodel.buildTower(tower_type = tower_type)
+    #mymodel.buildBedplate(bedplate_type = bedplate_type)
+    #mymodel.buildShaft(shaft_type = shaft_type)
+    #mymodel.buildMainBearing(MB1or2 = 'MB1', mb_type = mb_type)
+    #mymodel.buildMainBearing(MB1or2 = 'MB2', mb_type = mb_type)
+    print("Calculating nacelle mass and inertia")
+    mymodel.nacMassIner(add_nacelle_yaw_inertia = add_nacelle_yaw_inertia, add_yaw_br_mass = add_yaw_br_mass) 
+    print("Generating outputs")
+    mymodel.outputs()
+    print("Writing model")
+    mymodel.writeModel()
+    print("Writing json-file with member labels")
+    mymodel.writeJSON()
 
 # --- MAIN SCRIPT STARTS BELOW: ----------------------------------------------#
 if __name__ == '__main__':
